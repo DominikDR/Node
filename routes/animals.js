4b8f9c35831f8d7f6aeff860fdde26e1c58464b9
Dominik <dominik@pl.pl>
2018-10-06 15:00:11 +0200

    Deleted package.json library

--- routes/animals.js
+++ routes/animals.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const routes = express.Router();
 const sample = require('lodash/sample')
+const routes = express.Router();
 
 const species = ['elephant', 'cat', 'dog', 'snake', 'bee', 'bird'];
 
